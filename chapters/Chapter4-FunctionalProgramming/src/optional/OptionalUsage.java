6b839f92673231bde7998531ee98760b3fdcdbd4
Ruelos Joel <ruelosjoel@gmail.com>
2019-06-29 15:39:46 +0800

    Optional Usage

--- chapters/Chapter4-FunctionalProgramming/src/optional/OptionalUsage.java
+++ chapters/Chapter4-FunctionalProgramming/src/optional/OptionalUsage.java
@@ -20,15 +20,69 @@ public class OptionalUsage {
 		Optional<String> nullableName = Optional.ofNullable(nullValue);
 //		nullableName.get(); // throws Runtime Exception when optional is null!
 		System.out.println("Optional.ofNullable - isPresent: " + nullableName.isPresent());
-		
+
 		nullValue = "test";
 		nullableName.ifPresent(System.out::println);
 
 	}
 
+	static void createOptionalOrElse() {
+		String nullValue = null;
+
+		System.out.println("Get or else for null: " + Optional.ofNullable(nullValue).orElse(getDefault()));
+		// or else method is always invoke even if the the value is not null
+		System.out.println("Get or else for non null: " + Optional.ofNullable("Joel Ruelos").orElse(getDefault()));
+	}
+
+	static void createOptionalOrElseGet() {
+		String nullValue = null;
+		System.out.println("Get or else get: " + Optional.ofNullable(nullValue).orElseGet(OptionalUsage::getDefault));
+	}
+
+	static void createOptionalOrElseThrow() {
+		String value = null;
+		try {
+			System.out.println("Get or else throw: " + Optional.ofNullable(value).orElseThrow(() -> {
+				throw new IllegalArgumentException("cannot be null");
+			}));
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	static String getDefault() {
+		System.out.println("Getting default");
+		return "Default Name";
+	}
+	
+	static void createOptionalWithFilter() {
+		String name = "Joel";
+		System.out.println(Optional.ofNullable(name).filter(s -> s.startsWith("a")).orElse("Name doesn't start with 'a'!"));		
+		Integer age = 27;
+		System.out.println("Legal age? : " +Optional.ofNullable(age).filter(i -> i > 18).isPresent());
+	}
+	
+	static void createOptionalAndMap() {
+		String name = "Joel";
+		Optional.ofNullable(name).map(String::length).ifPresent(System.out::println);
+	}
+	
 	public static void main(String[] args) {
+		System.out.println("___________createEmptyOptional___________");
 		createEmptyOptional();
+		System.out.println("___________createOptionalOfString___________");
 		createOptionalOfString();
+		System.out.println("___________createOptionalOfNullableString___________");
 		createOptionalOfNullableString();
+		System.out.println("___________createOptionalOrElse___________");
+		createOptionalOrElse();
+		System.out.println("___________createOptionalOrElseGet___________");
+		createOptionalOrElseGet();
+		System.out.println("___________createOptionalOrElseThrow___________");
+		createOptionalOrElseThrow();
+		System.out.println("___________createOptionalWithFilter___________");
+		createOptionalWithFilter();
+		System.out.println("___________createOptionalAndMap___________");
+		createOptionalAndMap();
 	}
 }
