3f3512bc4b0e47994ea3a474a067df4e6a122f83
Ruelos Joel <ruelosjoel@gmail.com>
2019-07-12 23:27:53 +0800

    Instants

---
+++ chapters/Chapter5-DatesStringLocalization/src/datetimes/Instants.java
@@ -0,0 +1,36 @@
+package datetimes;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Instants {
+
+	static void create() {
+		System.out.println("--------Instant creation--------");
+		Instant i = Instant.now();
+		System.out.println("Instant now: " + i);
+		i = ZonedDateTime.now(ZoneId.systemDefault()).toInstant();
+		System.out.println("Zoned date time to instant: " + i);
+		i = Instant.ofEpochMilli(System.currentTimeMillis());
+		System.out.println("Instant of epoch milli: " + i);
+	}
+
+	static void manipulate() {
+		System.out.println("--------Manipulation--------");
+		Instant i = Instant.now();
+		i = i.plus(Period.ofDays(5)).plus(Duration.ofHours(1));
+		System.out.println("Plus period of 5 days and 1 hour: " + i);
+		i = i.plus(20, ChronoUnit.DAYS);
+		System.out.println("Plus 20 chrono unit day: " + i);
+	}
+
+	public static void main(String[] args) {
+		create();
+		manipulate();
+	}
+
+}
